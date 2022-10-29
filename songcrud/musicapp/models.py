a307a052313977fc42c5fc55ccd511a8986d72d9
Blessing <mac@Blessing-2.local>
2022-10-29 03:32:45 +0100

    added models to my app

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.age}'
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if len(self.content) > 50:
+            return f"{self.content[0:50]}..."
+        else:
+            return f"{self.content}"
